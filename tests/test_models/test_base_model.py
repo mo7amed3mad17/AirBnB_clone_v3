cee13e93fe9cb614b9b0263bc11694eb889d139e
mo7amed3mad17 <memo12317emad@gmail.com>
2024-08-05 14:56:35 +0200

    update

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,6 +3,7 @@
 from datetime import datetime
 import inspect
 import models
+import numpy as np
 import pep8 as pycodestyle
 import time
 import unittest
@@ -81,16 +82,31 @@ class TestBaseModel(unittest.TestCase):
     def test_datetime_attributes(self):
         """Test that two BaseModel instances have different datetime objects
         and that upon creation have identical updated_at and created_at
-        value."""
+        values."""
+        from models.base_model import BaseModel
         tic = datetime.now()
         inst1 = BaseModel()
         toc = datetime.now()
-        self.assertTrue(tic <= inst1.created_at <= toc)
-        time.sleep(1e-4)
+        self.assertTrue(np.allclose(
+            [inst1.created_at.timestamp()],
+            [tic.timestamp()],
+            atol=1e-6))
+        self.assertTrue(np.allclose(
+            [inst1.created_at.timestamp()],
+            [toc.timestamp()],
+            atol=1e-6))
+        time.sleep(0.001)
         tic = datetime.now()
         inst2 = BaseModel()
         toc = datetime.now()
-        self.assertTrue(tic <= inst2.created_at <= toc)
+        self.assertTrue(np.allclose(
+            [inst2.created_at.timestamp()],
+            [tic.timestamp()],
+            atol=1e-6))
+        self.assertTrue(np.allclose(
+            [inst2.created_at.timestamp()],
+            [toc.timestamp()],
+            atol=1e-6))
         self.assertEqual(inst1.created_at, inst1.updated_at)
         self.assertEqual(inst2.created_at, inst2.updated_at)
         self.assertNotEqual(inst1.created_at, inst2.created_at)
